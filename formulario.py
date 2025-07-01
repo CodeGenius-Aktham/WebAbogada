d4b9358f6ca185c8a55756d9dd38be44fb9a5aaf
Camilo <camilomunozb203@gmail.com>
2025-06-30 21:52:56 -0500

    Funcionalidad lista, mañana se integra la base de datos.

--- formulario.py
+++ formulario.py
@@ -1,7 +1,42 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from psycopg2 import IntegrityError
+import psycopg2
+import re
 
 app = Flask(__name__)
 CORS(app,origins='ejemplo.com',supports_credentials=True)
 
+@app.route('/customer', methods=['POST'])
+def ingreso_caso():
+    data = request.get_json()
 
+    nombre_cliente = data.get('nombre','').strip()
+    apellido_cliente =  data.get('apellido','').strip()
+    correo_usuario = data.get('correo', '').strip()
+    mensaje_usuario =  data.get('mensaje','').strip()
+
+    validador_email =  r"[a-zA-Z-0-9]+@[a-zA-Z]+\.[a-z-.]+$"
+
+    if not all[(nombre_cliente,apellido_cliente,correo_usuario,mensaje_usuario)]:
+        return jsonify({'error' : 'todos los campos deben estar completos'}), 400
+    elif not re.fullmatch(validador_email,correo_usuario):
+        return jsonify({'error' : 'el email ingreado no es correcto'}),400
+    
+    conn = conexion_db()
+    if not conn:
+        return jsonify({'error' : 'no se pudo conectar con la base de datos'})
+    try:
+        cusor = conn.cursor()
+        cusor.execute('''INSERT INTO clientes(nombre_cliente,apellido_cliente,correo_usuario,mensaje_usuario)
+                        VALUES (%s,%s,%s,%s)''',(nombre_cliente,apellido_cliente,correo_usuario,mensaje_usuario))
+        conn.commit()
+        return jsonify({'mensaje' : 'Ingreso del formulario correcto'}),200
+    except psycopg2.errors.UniqueViolation:
+        conn.rollback()
+        return jsonify({'error' : 'error de validacion de los datos'}),400
+    except Exception as error:
+        return jsonify({'error' : f'error inesperado en el sistema : {str(error)}'}),400
+    finally:
+        cusor.close()
+        conn.close()
