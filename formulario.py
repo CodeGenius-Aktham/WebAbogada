130855ca37c37e7ffd1041e00b5ee8940fdc752e
Camilo <camilomunozb203@gmail.com>
2025-07-02 19:53:55 -0500

    Base de datos lista falta conexion con el Fronted.

--- formulario.py
+++ formulario.py
@@ -1,11 +1,31 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from psycopg2 import IntegrityError
+from dotenv import load_dotenv
 import psycopg2
 import re
+import os
 
 app = Flask(__name__)
 CORS(app,origins='ejemplo.com',supports_credentials=True)
+
+load_dotenv()
+
+def conexion_db():
+    try:
+        conn = psycopg2.connect(
+            host = os.getenv('DB_HOST'),
+            name = os.getenv('DB_NAME'),
+            user = os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            port = os.getenv('DB_PORT'),
+            ssl =  os.getenv('DB_SSL')
+        )
+        return conn
+    except IntegrityError as error:
+        conn.rollback()
+        print(f"Error de conexion con la base de datos: {error}.")
+        return None
 
 @app.route('/customer', methods=['POST'])
 def ingreso_caso():
@@ -16,7 +36,7 @@ def ingreso_caso():
     correo_usuario = data.get('correo', '').strip()
     mensaje_usuario =  data.get('mensaje','').strip()
 
-    validador_email =  r"[a-zA-Z-0-9]+@[a-zA-Z]+\.[a-z-.]+$"
+    validador_email =  r"[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}"
 
     if not all[(nombre_cliente,apellido_cliente,correo_usuario,mensaje_usuario)]:
         return jsonify({'error' : 'todos los campos deben estar completos'}), 400
